313b33dd4eaae6f93fabb1e705119f1af8fcd7cf
Robert <robethuku11@gmail.com>
2023-03-26 15:04:53 +0300

    Completed lab

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+student = Student.create([{first_name: "Robert", last_name: "Thuku", grade: "B"}, {first_name: "Roberts", last_name: "kamau", grade: "B"}])
